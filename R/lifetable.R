d4a2e291a8b0851f199b0257674b12a5467cd0e3
gabrielfgm <gabriel.mesevage@gmail.com>
2023-12-22 16:09:19 +0200

    added license and function documentation as well as a brief project description

--- R/lifetable.R
+++ R/lifetable.R
@@ -1,4 +1,37 @@
-
+#' Construct a lifetable
+#'
+#' @param age A vector of ages or age ranges, e.g. '0-1', '1-4', '5-9', etc
+#' @param Dx A numeric vector of death counts for the age groups.
+#' @param Px A numeric vector of population sizes for the age groups.
+#'
+#' @return A dataframe containing a completed life-table.
+#' The column 'age' returns the age groups.
+#' Column 'mx' is the mortality rate at age x.
+#' Column 'ax' is the estimated mortality rate for individuals that die within the age range of x, e.g. individuals that die between '1-4' for age '1-4'.
+#' Column 'qx' is the probability of dying at age x.
+#' Column 'lx' is the magnitude of the population at x. It has a fixed radix of 1.
+#' Column 'dx' is the number of lifetable deaths.
+#' Column 'Lx' is the number of person years lived in age bin x.
+#' Column 'Tx' is the number of person years lived above age x.
+#' Column 'ex' is life-expectancy at age x.
+#' @export
+#'
+#' @examples
+#' # Age groups
+#' Age <-  c("0", "1-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34",
+#' "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69",
+#' "70-74", "75-79", "80-84", "85-89", "90-94", "95+")
+#'
+#' # Death counts
+#' Dx <- c(110934, 17693, 9168, 7378, 12192, 13356, 14223, 19210, 29136, 42961, 64310,
+#'         90630, 116759, 153504, 196682, 223796, 220065, 185305, 120414, 50299, 13887)
+#'
+#' # mid year population
+#' Px <- c(4126560, 16195304, 18659141, 16815965, 13287434, 10803165, 10870165,
+#'         11951709, 12508316, 11567216, 10528878, 9696502, 8595947, 7111897, 6186763,
+#'         4661136, 2977347, 1518206, 648581, 170653, 44551)
+#'
+#' lifetable(age = Age, Dx = Dx, Px = Px)
 lifetable <- function(age, Dx, Px) {
   # Death rates
   mx <- Dx / Px
@@ -35,7 +68,7 @@ lifetable <- function(age, Dx, Px) {
   ex <- Tx/lx
 
   # Create the life table
-  LT <- data.frame(age = Age, mx = mx, ax = ax, qx = qx,
+  LT <- data.frame(age = age, mx = mx, ax = ax, qx = qx,
                    lx = lx, dx = dx, Lx = Lx, Tx = Tx, ex = ex)
 
   LT
